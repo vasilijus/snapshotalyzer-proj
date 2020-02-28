275dc2fc265417150864b503fd86d374f63d0b5f
vasilijus <vasiliokas@gmail.com>
2020-02-28 22:00:17 +0000

    List & Touples

--- main.py
+++ main.py
@@ -11,7 +11,22 @@ def calc_test(arg1, arg2):
 
 
 print(result)
-
 calc_test(4,6)
-
 print(result)
+
+# Declaring a list 
+L = [1, "new", "quarry", 1+5]
+print("Printing list (L): ")
+print L
+L.append(123)
+print L
+L.pop()
+print L
+print("End of list...")
+
+# Declare Touples
+T = (1, "new", "touple", 2+3)
+print("Printing touple: ")
+print T
+
+
